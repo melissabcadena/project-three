76e9997ee823a2a6a836216d103c0f42cd8edba2
davidbelleza <davidbelleza@gmail.com>
2020-10-25 18:00:45 -0500

    login form start

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,19 @@ import { ThemeProvider } from '@chakra-ui/core';
 import customTheme  from '../src/theme/theme';
 
 //import Home from './pages/Home';
-import Menu from './pages/Menu';
+//import Menu from './pages/Menu';
 // import MyOrder from './pages/MyOrder';
 // import History from './pages/History';
 // import Signup from './pages/Signup';
-// import Login from './pages/Login';
+import Login from './pages/Login';
 
 
 function App() {
   return (
     <ThemeProvider theme={customTheme}>
       {/* <Home/> */}
-      <Menu/>
+      {/* <Menu/> */}
+      <Login/>
     </ThemeProvider>
     
   );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import {
+    Flex,
+    Box,
+    Heading,
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    ThemeProvider
+} from '@chakra-ui/core';
+import theme  from '../theme/theme';
+
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
 
 const Login = () => {
     return (
-        <div>
-            Login Page
-        </div>
+        <ThemeProvider theme={theme}>
+            <Nav/>
+            <Flex width="full" align="center" justifyContent="center" minHeight='100vh'>
+            <Box px={4} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg" textAlign="center">
+                <Box textAlign="center">
+                    <Heading>Login</Heading>
+                </Box>
+                <Box my={4} textAlign="left">
+                    <form>
+                        <FormControl>
+                            <FormLabel isRequired>Email</FormLabel>
+                            <Input type="email" placeholder="test@test.com" size="md"/>
+                        </FormControl>
+                        <FormControl mt={6}>
+                            <FormLabel isRequired>Password</FormLabel>
+                            <Input type="password" placeholder="*******" />
+                        </FormControl>
+                        <Button width="full" mt={4} type="submit">
+                        Log In
+                        </Button>
+                    </form>
+                </Box>
+            </Box>
+            </Flex>
+            <Footer/>
+        </ThemeProvider>
+
     );
 };
 
