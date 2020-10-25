3bcdd6d197e77309b7a78ffc65558d4d0c6375c4
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-25 15:56:34 -0500

    add switch in app.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { ThemeProvider, Flex, Box, Image, Stack, Heading, Button, Link, Text} from '@chakra-ui/core';
 import theme  from '../theme/theme';
 
-import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
 
 const Home = () => {
     return (
         <ThemeProvider theme={theme}>
-        <Nav/>
         <Flex
         align="center"
         justify={{ base: "center", md: "space-around", xl: "space-between" }}
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            Login Page
+            <h1>Login Page</h1>
         </div>
     );
 };
