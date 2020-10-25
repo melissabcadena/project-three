262cc6a06da10e7637cc5cb6085e19e09f3dc159
davidbelleza <davidbelleza@gmail.com>
2020-10-24 19:28:58 -0500

    add theme page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { ThemeProvider } from '@chakra-ui/core';
+import customTheme  from '../src/theme/theme';
 
 import Home from './pages/Home';
-import Menu from './pages/Menu';
-import MyOrder from './pages/MyOrder';
-import History from './pages/History';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
+// import Menu from './pages/Menu';
+// import MyOrder from './pages/MyOrder';
+// import History from './pages/History';
+// import Signup from './pages/Signup';
+// import Login from './pages/Login';
 
 
 function App() {
   return (
-    
-    <div className="App">
+    <ThemeProvider theme={customTheme}>
       <Home></Home>
-      <Menu></Menu>
-      <MyOrder></MyOrder>
-      <History></History>
-      <Signup></Signup>
-      <Login></Login>
-    </div>
+    </ThemeProvider>
+    
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ThemeProvider, Box } from '@chakra-ui/core';
+import theme  from '../theme/theme';
 
 
 const Home = () => {
     return (
+        <ThemeProvider theme={theme}>
+        <Box bg="brown.1" w="100%" p={4} color="white">
+            Brand Name
+        </Box>
         <div>
             Home Page
         </div>
+        </ThemeProvider>
     );
 };
 
