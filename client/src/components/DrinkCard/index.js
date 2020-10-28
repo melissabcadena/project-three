4b9fc24e52f205b8ce8d41a01efa2412535fcbb8
Cherie Cookson <cheriecookson@gmail.com>
2020-10-27 21:22:52 -0500

    added collapse and options to drinkcard

--- client/src/components/DrinkCard/index.js
+++ client/src/components/DrinkCard/index.js
@@ -1,23 +1,57 @@
 import React from 'react';
-import { Box, Image, } from '@chakra-ui/core';
+import { Box, Image, Button, Collapse, FormControl, FormLabel, Radio, RadioGroup } from '@chakra-ui/core';
 
 function DrinkCard() {
-    return(
+
+    const [show, setShow] = React.useState(false);
+
+    const handleToggle = () => setShow(!show);
+
+    return (
+
         <Box maxW="sm" borderWidth="2px" bg="white.2">
             <Image src="https://via.placeholder.com/200" />
             <Box p="6">
                 <Box mt="1" fontWeight="semibold" as="h3" lineHeight="tight" isTruncated>
                     Coffee
-                </Box>
+                    </Box>
                 <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
                     Price
-                </Box>
+                    </Box>
                 <Box as="span" fontSize="sm">
                     Description of the coffee
-                </Box>
+                    </Box>
             </Box>
+            <Button variantColor="primary"
+                borderRadius="8px"
+                py="4"
+                px="4"
+                lineHeight="1"
+                size="md" onClick={handleToggle}>
+                Customize
+                </Button>
+            <Collapse mt={4} isOpen={show}>
+                <FormControl as="fieldset">
+                    <FormLabel as="legend">Select Size</FormLabel>
+                    <RadioGroup defaultValue="Small">
+                        <Radio value="Small">Small</Radio>
+                        <Radio value="Large">Large</Radio>
+                    </RadioGroup>
+                    </FormControl>
+                    <FormControl as="fieldset">
+                    <FormLabel as="legend">Add Milk</FormLabel>
+                    <RadioGroup defaultValue="No Milk">
+                        <Radio value="No Milk">No Milk</Radio>
+                        <Radio value="Milk">Milk</Radio>
+                    </RadioGroup>            
+                </FormControl>
+            </Collapse>
         </Box>
-    )
+    );
 }
+
+
+
+
 
 export default DrinkCard;
