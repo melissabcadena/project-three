6279dbc68696dd99354f61ce668144f098159802
Melissa Cadena <melissabcadena@Melissas-MacBook-Pro.local>
2020-10-29 13:23:36 -0500

    add drinks to idb

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -8,8 +8,7 @@ import DrinkCard from '../components/DrinkCard';
 import { QUERY_ALL_DRINKS } from '../utils/queries';
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_DRINKS } from "../utils/actions";
-
-
+import { idbPromise } from "../utils/helpers";
 
 const Menu = () => {
     const [state, dispatch] = useStoreContext();
@@ -21,6 +20,17 @@ console.log("state", state);
                 type: UPDATE_DRINKS,
                 drinks: data.drinks
             });
+
+            data.drinks.forEach((item) => {
+                idbPromise('drinks', 'put', item);
+            });
+        } else if (!loading) {
+            idbPromise('drinks', 'get').then((drinks) => {
+                dispatch({
+                    type: UPDATE_DRINKS, 
+                    drinks: drinks
+                })
+            })
         }
     }, [loading, data, dispatch]);
     console.log(state.drinks);
