125173aad14bb306f3882dc882949bdb2a96e549
Sylvia <sylviaprabudy@gmail.com>
2020-11-01 12:41:29 -0600

    fix sucess page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -11,13 +11,13 @@ function Success() {
     useEffect(() => {
         async function saveOrder() {
             const cart = await idbPromise('cart', 'get');
-            const products = cart.map(item => item._id);
+            const drinks = cart.map(item => item._id);
 
-            if (products.length) {
-                const { data } = await addOrder({ variables: { products } });
-                const productData = data.addOrder.products;
+            if (drinks.length) {
+                const { data } = await addOrder({ variables: { drinks } });
+                const drinkData = data.addOrder.drinks;
 
-                productData.forEach((item) => {
+                drinkData.forEach((item) => {
                     idbPromise('cart', 'delete', item);
                 });
             }
